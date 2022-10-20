b4c521ea25b5fce233e94fe673a82b69996b5861
Falk Werner <falk.werner@gmx.net>
2022-10-21 00:25:21 +0200

    fixed URL

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-VERSION=03.07.14
+VERSION=v03.07.14
 OSS_PACKAGES_URL=https://github.com/falk-werner/pfc-sdk-g2/releases/download/${VERSION}/open-source-packages.tar.xz
 CSS_PACKAGES_URL=https://github.com/falk-werner/pfc-sdk-g2/releases/download/${VERSION}/closed-source-packages.tar.xz
 
